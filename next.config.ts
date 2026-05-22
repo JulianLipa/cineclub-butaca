bbd12345fb7cbafbf1491c7f845a842200880d22
JulianLipa <lipajulianp@gmail.com>
2026-05-22 13:36:57 -0300

    vote banner + reviews v3

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   allowedDevOrigins: ["*.ngrok-free.app"],
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;

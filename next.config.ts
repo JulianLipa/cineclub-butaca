e6f010a5a3e494e46c433d81bb0f9e62ffaf40d1
JulianLipa <lipajulianp@gmail.com>
2026-05-22 13:32:30 -0300

    vote banner + reviews v2

--- next.config.ts
+++ next.config.ts
@@ -3,23 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   allowedDevOrigins: ["*.ngrok-free.app"],
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: [
-          {
-            loader: "@svgr/webpack",
-            options: {
-              svgoConfig: {
-                plugins: ["removeDimensions"],
-              },
-            },
-          },
-        ],
-        as: "*.js",
-      },
-    },
-  },
 };
 
 export default nextConfig;
